thatguystone/lm-sensors

commit 1bd92120659febdcd19bfa7dbc3722fe00e7b753

Fill the magnitude field of dynamically generated device entries.
git-svn-id: http://lm-sensors.org/svn/lm-sensors/branches/lm-sensors-3.0.0@4420 7894878c-1315-0410-8ee3-d5d059ff63e0

lib/sysfs.c
@@ -37,6 +37,29 @@ char sensors_sysfs_mount[NAME_MAX];
 
 #define MAX_SENSORS_PER_TYPE 16
 
+static
+int get_type_scaling(int type)
+{
+	switch (type & 0xFF10) {
+	case SENSORS_FEATURE_IN:
+	case SENSORS_FEATURE_TEMP:
+		return 3;
+	case SENSORS_FEATURE_FAN:
+		return 0;
+	}
+
+	switch (type) {
+	case SENSORS_FEATURE_VID:
+		return 3;
+	case SENSORS_FEATURE_VRM:
+		return 1;
+	default:
+		return 0;
+	}
+
+	return 0;
+}
+
 static 
 sensors_chip_features sensors_read_dynamic_chip(struct sysfs_device *sysdir)
 {
@@ -148,6 +171,8 @@ sensors_chip_features sensors_read_dynamic_chip(struct sysfs_device *sysdir)
 			SENSORS_MODE_RW : (attr->method & SYSFS_METHOD_SHOW) ?
 			SENSORS_MODE_R : (attr->method & SYSFS_METHOD_STORE) ?
 			SENSORS_MODE_W : SENSORS_MODE_NO_RW;
+
+		feature.scaling = get_type_scaling(type);
 
 		features[i] = feature;
 		fnum++;

commit b76cd8a5f62ff304480c1088bd020d186d6eaee5

sysfs: Specific match has to go before generic match
This is not really a problem when the specific scaling factors happen to
be for factors not matched by a generic match but this may not hold in
general over time.

Fixes: 1bd92120 ("Fill the magnitude field of dynamically generated device entries.")

lib/sysfs.c
@@ -141,6 +141,17 @@ char sensors_sysfs_mount[NAME_MAX];
 static
 int get_type_scaling(sensors_subfeature_type type)
 {
+	/* Multipliers for second class subfeatures
+	   that need their own multiplier */
+	switch (type) {
+	case SENSORS_SUBFEATURE_POWER_AVERAGE_INTERVAL:
+	case SENSORS_SUBFEATURE_VID:
+	case SENSORS_SUBFEATURE_TEMP_OFFSET:
+		return 1000;
+	default:
+		break;
+	}
+
 	/* Multipliers for subfeatures */
 	switch (type & 0xFF80) {
 	case SENSORS_SUBFEATURE_IN_INPUT:
@@ -157,16 +168,7 @@ int get_type_scaling(sensors_subfeature_type type)
 		return 1000000;
 	}
 
-	/* Multipliers for second class subfeatures
-	   that need their own multiplier */
-	switch (type) {
-	case SENSORS_SUBFEATURE_POWER_AVERAGE_INTERVAL:
-	case SENSORS_SUBFEATURE_VID:
-	case SENSORS_SUBFEATURE_TEMP_OFFSET:
-		return 1000;
-	default:
-		return 1;
-	}
+	return 1;
 }
 
 static