thatguystone/lm-sensors

commit 8896ba3b1f3ca6c0cd59c34abfe9f8fa67f93956

Rename sensors_chip_name.busname to path, as this is what we are using it for by now.
git-svn-id: http://lm-sensors.org/svn/lm-sensors/branches/lm-sensors-3.0.0@4673 7894878c-1315-0410-8ee3-d5d059ff63e0

lib/data.c
@@ -78,9 +78,6 @@ int sensors_parse_chip_name(const char *orig_name, sensors_chip_name *res)
   char *name = strdup(orig_name);
   int i;
 
-  /* Play it safe */
-  res->busname = NULL;
-  
   if (! name)
     sensors_fatal_error("sensors_parse_chip_name","Allocating new name");
   /* First split name in upto four pieces. */

lib/access.c
@@ -170,7 +170,7 @@ int sensors_get_label(const sensors_chip_name *name, int feature, char **result)
 			}
 
 	/* No user specified label, check for a _label sysfs file */
-	snprintf(path, PATH_MAX, "%s/%s_label", name->busname,
+	snprintf(path, PATH_MAX, "%s/%s_label", name->path,
 		featureptr->data.name);
 	
 	if ((f = fopen(path, "r"))) {

lib/init.c
@@ -90,7 +90,7 @@ void sensors_cleanup(void)
 void free_chip_name(sensors_chip_name *name)
 {
   free(name->prefix);
-  free(name->busname);
+  free(name->path);
 }
 
 void free_chip_features(sensors_chip_feature *features)

lib/sensors.h
@@ -43,7 +43,7 @@ typedef struct sensors_chip_name {
   char *prefix;
   int bus;
   int addr;
-  char *busname;
+  char *path;
 } sensors_chip_name;
 
 /* (Re)load the configuration file and the detected chips list. If this 

lib/sysfs.c
@@ -222,8 +222,8 @@ static int sensors_read_one_sysfs_chip(struct sysfs_device *dev)
 	if (!entry.chip.prefix)
 		sensors_fatal_error(__FUNCTION__, "out of memory");
 
-	entry.chip.busname = strdup(dev->path);
-	if (!entry.chip.busname)
+	entry.chip.path = strdup(dev->path);
+	if (!entry.chip.path)
 		sensors_fatal_error(__FUNCTION__, "out of memory");
 
 	if (sscanf(dev->name, "%d-%x", &entry.chip.bus, &entry.chip.addr) == 2) {
@@ -270,7 +270,7 @@ static int sensors_read_one_sysfs_chip(struct sysfs_device *dev)
 
 exit_free:
 	free(entry.chip.prefix);
-	free(entry.chip.busname);
+	free(entry.chip.path);
 	return err;
 }
 
@@ -416,7 +416,7 @@ int sensors_read_sysfs_attr(const sensors_chip_name *name, int feature,
 	 || sscanf(the_feature->data.name, "temp%d%c", &dummy, &check) == 1)
 		suffix = "_input";
 
-	snprintf(n, NAME_MAX, "%s/%s%s", name->busname, the_feature->data.name,
+	snprintf(n, NAME_MAX, "%s/%s%s", name->path, the_feature->data.name,
 		 suffix);
 	if ((f = fopen(n, "r"))) {
 		int res = fscanf(f, "%lf", value);
@@ -451,7 +451,7 @@ int sensors_write_sysfs_attr(const sensors_chip_name *name, int feature,
 	 || sscanf(the_feature->data.name, "temp%d%c", &dummy, &check) == 1)
 		suffix = "_input";
 
-	snprintf(n, NAME_MAX, "%s/%s%s", name->busname, the_feature->data.name,
+	snprintf(n, NAME_MAX, "%s/%s%s", name->path, the_feature->data.name,
 		 suffix);
 	if ((f = fopen(n, "w"))) {
 		for (mag = the_feature->scaling; mag > 0; mag --)

commit 10c6068b3ed5fac90d7a2bf3b76a97496632e97d

sensors_parse_chip_name: Initialize path
sensors_parse_chip_name claims to overwrite all of result but path is
not set. This is generally not a problem when using statically allocated
data that is pre-zeroed but the parser uses dynamic allocation and the
chip name may contain garbage depending on the specific code flow
resulting from compiling the parser.
Setting the path was dropped when it was renamed from busname.

Fixes: 8896ba3b ("Rename sensors_chip_name.busname to path, as this is
		  what we are using it for by now.")

lib/data.c
@@ -84,6 +84,7 @@ int sensors_parse_chip_name(const char *name, sensors_chip_name *res)
 {
 	char *dash;
 
+	memset(res, 0, sizeof(*res));
 	/* First, the prefix. It's either "*" or a real chip name. */
 	if (!strncmp(name, "*-", 2)) {
 		res->prefix = SENSORS_CHIP_NAME_PREFIX_ANY;